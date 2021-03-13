ac340986029473abee0fe52bd51f2bdfa718cc52
Amanda-Katherine <walker.akw@gmail.com>
2021-03-12 23:06:32 -0500

    import and declare Servers Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component'; //without .ts...this is added by webpack which bundles the product automatically
+import { ServersComponent } from './servers/servers.component';  
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent
   ],
   imports: [
     BrowserModule
